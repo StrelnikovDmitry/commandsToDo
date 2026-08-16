d20e10cd14426426ef6abcaf60d08422d4562ba7
StrelnikovDmitry <Formeazaz@gmail.com>
2026-08-16 19:56:17 +0700

    add print_list function, fix bug
    add print_list function which prints every element of the list
    
    fix bug off size (in element) being used in functions without
    multiplying by element_byte_size var

--- src/list/list.c
+++ src/list/list.c
@@ -9,10 +9,16 @@ int* create_list(int basic_size) {
 
 int* expand_list(int* pointer) {
     size *= 2;
-    return realloc(pointer, size*);
+    return realloc(pointer, size*element_byte_size);
 }
 
 int* shrink_list(int* pointer) {
     size /= 2;
     return realloc(pointer, size*element_byte_size);
+}
+
+void print_list(int* pointer) {
+    for (int i = 0; i < size; i++) {
+        printf("%d, ", pointer[i]);
+    }
 }
