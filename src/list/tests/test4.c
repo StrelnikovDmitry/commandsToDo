8c837705f0016c7007cede77de5dd8bd67615e61
StrelnikovDmitry <Formeazaz@gmail.com>
2026-08-17 00:45:34 +0700

    create test4.c
    test4.c checks the stability of the deleting system working

---
+++ src/list/tests/test4.c
@@ -0,0 +1,19 @@
+#include "../list_functions.h"
+
+int main() {
+    int* list = create_list(2);
+    list[1] = 12;
+
+    add_element(list, 1);
+    add_element(list, 2);
+    add_element(list, 3);
+
+    print_list(list);
+
+    delete_element(list, 2);
+    delete_element(list, 5);
+
+    print_list(list);
+
+    return 0;
+}
