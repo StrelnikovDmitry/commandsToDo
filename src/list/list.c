9838e8a66dda638871ab0a7ede33f85162d80c5d
StrelnikovDmitry <Formeazaz@gmail.com>
2026-08-17 00:30:39 +0700

    fix bug
    add_element functionss does now rewrite manually assigned elements

--- src/list/list.c
+++ src/list/list.c
@@ -17,11 +17,14 @@ int* shrink_list(int* pointer) {
     return realloc(pointer, size*element_byte_size);
 }
 
-
 void add_element(int* pointer, int value) {
-    if (cursor == size-1) {
+    while(pointer[cursor] != 0) {
+        cursor++;
+    }
+    if (cursor == size) {
         pointer = expand_list(pointer);
     }
+
     pointer[cursor] = value;
     cursor++;
 }
